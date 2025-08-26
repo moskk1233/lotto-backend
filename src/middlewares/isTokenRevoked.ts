4e9b32147d1b9631b7d5225af80a5556ca240b9f
Nonpawit P. <nonpawit.p@outlook.com>
2025-08-26 15:24:33 +0700

    feat: add logout

---
+++ src/middlewares/isTokenRevoked.ts
@@ -0,0 +1,22 @@
+import { createMiddleware } from 'hono/factory';
+import redis from '../redis.js';
+
+export const isTokenRevoked = createMiddleware(async (c, next) => {
+  const authorizationHeader = c.req.header('Authorization');
+  const token = authorizationHeader!.split(' ')[1];
+
+  if (await redis.exists(`revoked_token:${token}`)) {
+    return c.json(
+      {
+        error: {
+          status: 401,
+          code: 'UNAUTHORIZED',
+          detail: 'Invalid token or revoked',
+        },
+      },
+      401,
+    );
+  }
+
+  await next();
+});
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,6 +5,9 @@ import { UserService } from '../services/users.js';
 import argon2 from 'argon2';
 import { signToken } from '../jwt.js';
 import { Duration } from '../utils.js';
+import { jwtMiddleware } from '../middlewares/jwtMiddleware.js';
+import redis from '../redis.js';
+import { isTokenRevoked } from '../middlewares/isTokenRevoked.js';
 
 const route = new Hono();
 const userService = UserService.getInstance();
@@ -84,8 +87,28 @@ route.post(
   },
 );
 
-// route.delete("/token", async (c) => {
-// TODO: Revoke JWT by used redis to cache revoked token
-// });
+route.delete('/token', jwtMiddleware, isTokenRevoked, async (c) => {
+  try {
+    const authorizationHeader = c.req.header('Authorization');
+    const token = authorizationHeader!.split(' ')[1];
+    const userJwtClaim = c.get('jwtPayload') as { exp: number };
+    const now = Math.floor(Date.now() / 1000);
+    const result = await redis.setEx(`revoked_token:${token}`, userJwtClaim.exp - now, 'revoked');
+
+    if (result !== 'OK') throw new Error();
+    return c.body(null, 204);
+  } catch {
+    return c.json(
+      {
+        error: {
+          status: 500,
+          code: 'INTERNAL_SERVER_ERROR',
+          detail: 'Something went wrong please try again',
+        },
+      },
+      500,
+    );
+  }
+});
 
 export default route;
