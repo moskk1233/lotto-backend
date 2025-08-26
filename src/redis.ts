b1fb5da276eeee2e66978e50c9b0fba1ec0ff0aa
Nonpawit P. <nonpawit.p@outlook.com>
2025-08-26 17:14:40 +0700

    chore(redis): add url instead of default url

--- src/redis.ts
+++ src/redis.ts
@@ -1,6 +1,14 @@
 import { createClientPool } from 'redis';
 
-const redis = createClientPool();
+const redis = createClientPool(
+  {
+    url: process.env.REDIS_URL!,
+  },
+  {
+    minimum: 1,
+    maximum: 5,
+  },
+);
 
 redis.on('error', (err) => console.log('Redis client connection error:', err));
 
