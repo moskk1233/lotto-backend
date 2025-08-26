134b4bae26d5efd95099bc3bf9b66f32bcd17492
Nonpawit P. <nonpawit.p@outlook.com>
2025-08-26 14:03:45 +0700

    chore: add format script for prisma schema

--- .lintstagedrc.ts
+++ .lintstagedrc.ts
@@ -3,6 +3,7 @@ import { type Configuration } from 'lint-staged';
 const config: Configuration = {
   '*.{ts,mts,js,mjs}': 'prettier --write',
   '{src/**}.{mjs,js,cjs,ts,tsx}': 'eslint --fix',
+  'prisma/schema.prisma': 'prisma format',
 };
 
 export default config;
