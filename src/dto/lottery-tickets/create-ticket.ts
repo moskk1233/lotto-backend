464ba07e19d376c816a1c8cef2e8c2d90eb1b2c2
Nonpawit P. <nonpawit.p@outlook.com>
2025-08-30 01:49:32 +0700

    fix: forgot to add price to ticket

--- src/dto/lottery-tickets/create-ticket.ts
+++ src/dto/lottery-tickets/create-ticket.ts
@@ -3,6 +3,7 @@ import z from 'zod';
 export const createTicketSchema = z.object({
   ticketNumber: z.string().min(6).max(6),
   ownerId: z.int().optional(),
+  price: z.int(),
 });
 
 export type CreateTicketDto = z.infer<typeof createTicketSchema> & {
