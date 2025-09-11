20a5b3ea151205275a1320e756d949d896840ca8
Nonpawit P. <nonpawit.p@outlook.com>
2025-09-11 22:48:42 +0700

    fix(users): remove unused property

--- src/dto/users/search-user-query.dto.ts
+++ src/dto/users/search-user-query.dto.ts
@@ -1,14 +1,12 @@
 import { Type } from 'class-transformer';
-import { IsNumber, IsOptional, Min } from 'class-validator';
+import { IsNumber, Min } from 'class-validator';
 
 export class SearchUserQueryDto {
-  @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(1)
   page: number = 1;
 
-  @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(1)
