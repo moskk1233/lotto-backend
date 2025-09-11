c89c0e7eb26d0c769edf371f1b98641682af999b
Nonpawit P. <nonpawit.p@outlook.com>
2025-09-11 22:33:42 +0700

    fix(main): add transform option to validation pipe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AllExceptionFilter } from './middlewares/all-exception/all-exception.fi
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new AllExceptionFilter());
 
   const config = new DocumentBuilder()
