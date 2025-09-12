d0035e76016f9e30feac0c83610fa981cb3d561e
Nonpawit P. <nonpawit.p@outlook.com>
2025-09-12 17:21:44 +0700

    docs: add swagger property detail

--- src/dto/auth/login.dto.ts
+++ src/dto/auth/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({ example: 'johndoe' })
   @IsString()
   username: string;
 
+  @ApiProperty({ example: '123' })
   @IsString()
   password: string;
 }
--- src/dto/users/update-user.dto.ts
+++ src/dto/users/update-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsEnum,
@@ -9,29 +10,35 @@ import {
 import { UserRoleEnum } from 'src/common/enums/user-role.enum';
 
 export class UpdateUserDto {
+  @ApiProperty({ example: 'johndoe' })
   @IsOptional()
   @IsString()
   password: string;
 
+  @ApiProperty({ example: 1000 })
   @IsOptional()
   @IsNumber()
   money: number;
 }
 
 export class UpdateUserByAdminDto extends UpdateUserDto {
+  @ApiProperty({ example: 'johndoe' })
   @IsOptional()
   @IsString()
   username: string;
 
+  @ApiProperty({ example: 'johndoe@mail.com' })
   @IsOptional()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: '0888888888' })
   @IsOptional()
   @IsString()
   @Length(10, 10)
   phone: string;
 
+  @ApiProperty({ examples: ['user', 'admin'] })
   @IsOptional()
   @IsEnum(UserRoleEnum)
   role: UserRoleEnum;
