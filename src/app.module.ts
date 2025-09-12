3bd68d4945bfcb9232ef247bd5d429389703a82d
Nonpawit P. <nonpawit.p@outlook.com>
2025-09-12 16:24:47 +0700

    feat(auth): add sign in

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,30 @@ import { AppService } from './app.service';
 import { PrismaService } from './services/prisma/prisma.service';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '1h',
+          algorithm: 'HS512',
+          issuer: 'Lotto 888',
+        },
+        verifyOptions: { algorithms: ['HS512'] },
+      }),
+    }),
+  ],
+  controllers: [AppController, UsersController, AuthController],
   providers: [AppService, PrismaService, UsersService],
 })
 export class AppModule {}
---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { LoginDto } from 'src/dto/auth/login.dto';
+import { Unauthorized } from 'src/exceptions/unauthorized/unauthorized';
+import { UsersService } from 'src/services/users/users.service';
+import argon2 from 'argon2';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtService } from '@nestjs/jwt';
+
+@ApiTags('Auth')
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  @Post('token')
+  @HttpCode(HttpStatus.OK)
+  async signIn(@Body() loginDto: LoginDto) {
+    const { username, password } = loginDto;
+
+    const existedUser = await this.userService.getByUsername(username);
+    if (!existedUser) throw new Unauthorized('username or password is invalid');
+
+    const isVerified = await argon2.verify(existedUser.password, password);
+    if (!isVerified) throw new Unauthorized('username or password is invalid');
+
+    const token = await this.jwtService.signAsync({
+      userId: existedUser.id,
+      role: existedUser.role,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
